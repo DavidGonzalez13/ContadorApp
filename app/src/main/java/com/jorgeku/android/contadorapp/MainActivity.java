31a56b3962d31de8695827f43848c439310e4650
Jorge David Ku Gonzalez <14470427@itcampeche.edu.mx>
2017-10-09 12:08:06 -0500

    ContadorApp: Se agregó el manejo de eventos a la

--- app/src/main/java/com/jorgeku/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/jorgeku/android/contadorapp/MainActivity.java
@@ -2,12 +2,38 @@ package com.jorgeku.android.contadorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBotonContar, mBotonReiniciar;
+    private EditText mTextoContar;
+    int mcontar = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mBotonContar = (Button)findViewById(R.id.BotonContar);
+        mBotonReiniciar = (Button)findViewById(R.id.BotonReiniciar);
+        mTextoContar = (EditText) findViewById(R.id.Incrementa);
+
+        mBotonContar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcontar++;
+                mTextoContar.setText(mcontar+"");
+
+            }
+        });
+
+
+
+
     }
 }
